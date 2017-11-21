0c2eb75d831a298778bf498ce5e7f8de48a92f9e
Courtney Fay <fay.courtney@gmail.com>
2017-11-21 15:59:42 -0700

    finishes spring 5 and can now delete todos

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -36,6 +36,16 @@ class TodosContainer extends Component {
 		});
 	}
 
+	deleteTodo(todo) {
+		console.log('deleting todo', todo);
+		TodoModel.delete(todo).then((res) => {
+			let todos = this.state.todos.filter(function(todo) {
+				return todo._id !== res._id;
+			});
+			this.setState({todos});
+		});
+	}
+
 	render(){
 		return (
 			<div className='todos-container'>
@@ -43,7 +53,7 @@ class TodosContainer extends Component {
 					createTodo={this.createTodo.bind(this)} />
 				<TodoList
 					todos={this.state.todos}
-				/>
+					onDeleteTodo={this.deleteTodo.bind(this)} />
 			</div>
 		)
 	}
