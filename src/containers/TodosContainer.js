989685df230d800ed8517e3d0662d8fc59136937
Courtney Fay <fay.courtney@gmail.com>
2017-11-21 12:26:26 -0700

    finishes sprint 4 and can now add data to the super crud woo

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import TodoModel from '../models/Todo';
 import TodoList from '../components/TodoList';
+import CreateTodoForm from '../components/CreateTodoForm';
 
 class TodosContainer extends Component {
 	constructor() {
@@ -22,9 +23,24 @@ class TodosContainer extends Component {
 		});
 	}
 
+	createTodo(newBody) {
+		let newTodo = {
+			body: newBody,
+			completed: false
+		}
+		TodoModel.create(newTodo).then((res) => {
+			console.log('created todo', res);
+			let todos = this.state.todos;
+			let newTodos = todos.push(res);
+			this.setState({newTodos});
+		});
+	}
+
 	render(){
 		return (
 			<div className='todos-container'>
+				<CreateTodoForm 
+					createTodo={this.createTodo.bind(this)} />
 				<TodoList
 					todos={this.state.todos}
 				/>
